720563e7efc8d87c0770c415abf1170bbc5cff96
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2023-07-09 14:05:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,6 +49,8 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 
+LANGUAGES = ["malayalam", "tamil", "english", "hindi", "telugu", "kannada"]
+
 
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
