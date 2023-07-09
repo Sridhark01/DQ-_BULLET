b8e966f91679829a5189684003857054778d8814
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2023-07-09 13:40:50 +0530

    Delete clone_plugins directory

--- clone_plugins/custom.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client, filters 
-from database.users_chats_db import db
-
-@Client.on_message(filters.private & filters.command('set_start'))
-async def set_start_message(c: Client, message, strings):
-    if len(m.text.split()) > 1:
-        message = message.text.html.split(None, 1)[1]
-    await message.reply_text("Yᴏᴜʀ Sᴛᴀʀᴛ Mᴇssᴀɢᴇ Sᴜᴄᴄᴇssғᴜʟʟʏ ᴀᴅᴅᴇᴅ")
