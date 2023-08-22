c77ad3c7ee4c3f577200f5bd1ab806724907e560
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2023-08-22 07:09:01 +0530

    Update Torrent.py

--- plugins/more/Torrent.py
+++ plugins/more/Torrent.py
@@ -50,7 +50,7 @@ async def find(_, message):
         f"➲Leechers: {a[i]['Leechers']} || "
         f"➲Seeders: {a[i]['Seeders']}\n"
         f"➲Type: {a[i]['Category']}\n"
-        f"➲Magnet: `<code>{a[i]['Magnet']}</code>`\n\n\n"
+        f"➲Magnet:\n```{a[i]['Magnet']}```\n\n\n"
     )
     await m.edit(
         result,
