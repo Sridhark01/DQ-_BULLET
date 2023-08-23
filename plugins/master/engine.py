195812d1ba57c7f820d0b619832f6ba0910c52da
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2023-08-23 22:58:29 +0530

    Update engine.py

--- plugins/master/engine.py
+++ plugins/master/engine.py
@@ -1,7 +1,7 @@
 import openai
 
 async def ai(query):
-    openai.api_key = "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" #Your openai api key
+    openai.api_key = "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" #Your openai api key
     response = openai.Completion.create(engine="text-davinci-002", prompt=query, max_tokens=100, n=1, stop=None, temperature=0.9, timeout=5)
     return response.choices[0].text.strip()
      
