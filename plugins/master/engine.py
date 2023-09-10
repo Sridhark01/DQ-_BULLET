f138b19763d3b6b862f1cb73b3e824c154cb9ddf
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2023-09-10 07:01:40 +0530

    Update engine.py

--- plugins/master/engine.py
+++ plugins/master/engine.py
@@ -1,7 +1,7 @@
 import openai
 
 async def ai(query):
-    openai.api_key = "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" #Your openai api key
+    openai.api_key = "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" #Your openai api key
     response = openai.Completion.create(engine="text-davinci-002", prompt=query, max_tokens=100, n=1, stop=None, temperature=0.9, timeout=5)
     return response.choices[0].text.strip()
      
@@ -11,7 +11,7 @@ async def ask_ai(client, m, message):
         # Generate response using OpenAI API
         response = await ai(question)
         # Send response back to user
-        await m.edit(f"{response}")
+        await m.edit(f"ᴀsᴋᴇᴅ ʙʏ📫: {message.from_user.mention}\nǫᴜᴇʀʏ🔖: {question}\n\n{response}")
     except Exception as e:
         # Handle other errors
         error_message = f"An error occurred: {e}"
