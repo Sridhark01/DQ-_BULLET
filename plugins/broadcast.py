6f0e0773d8efce1d839677cbef79103fd900f732
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2023-10-10 10:45:59 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -30,29 +30,31 @@ async def users_broadcast(bot, message):
     total_users = await db.total_users_count()
     done = 0
     blocked = 0
-    failed =0
+    failed = 0
     temp.USERS_CANCEL = False
     success = 0
-        
+
     async for user in users:
         if temp.USERS_CANCEL:
-            await msg.edit(f"Users Successfully Broadcast Canceled!")
+            await sts.edit(f"Users Successfully Broadcast Canceled!")
             break
-        sts = await broadcast_messages(int(user['id']), b_msg)
-        if sts == 'Success':
+        result = await broadcast_messages(int(user['id']), b_msg)
+        if result == 'Success':
             success += 1
-        elif sts == 'Blocked':
-            blocked+=1
-        elif sts == 'Error':
+        elif result == 'Blocked':
+            blocked += 1
+        elif result == 'Error':
             failed += 1
         done += 1
         if not done % 20:
             btn = [[
                 InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#users')
             ]]
-            await sts.edit(f"Users broadcast in progress...\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>\nBlocked: <code>{blocked}</code>", reply_markup=InlineKeyboardMarkup(btn))    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+            await sts.edit(f"Users broadcast in progress...\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>\nBlocked: <code>{blocked}</code>", reply_markup=InlineKeyboardMarkup(btn))
+    
+    time_taken = datetime.timedelta(seconds=int(time.time() - start_time))
     await sts.edit(f"Users broadcast completed.\nCompleted in {time_taken} seconds.\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>\nBlocked: <code>{blocked}</code>")
+
         
 
 @Client.on_message(filters.command(["grp_broadcast", "gb"]) & filters.user(ADMINS) & filters.reply)
@@ -65,24 +67,26 @@ async def groups_broadcast(bot, message):
     start_time = time.time()
     total_chats = await db.total_chat_count()
     done = 0
-    failed =0
+    failed = 0
     temp.GROUPS_CANCEL = False
     success = 0
-        
+
     async for chat in chats:
         if temp.GROUPS_CANCEL:
             await msg.edit(f"Groups Successfully Broadcast Canceled!")
             break
-        sts = await groups_broadcast_messages(int(chat['id']), b_msg)
-        if sts == 'Success':
+        result = await groups_broadcast_messages(int(chat['id']), b_msg)
+        if result == 'Success':
             success += 1
-        elif sts == 'Error':
+        elif result == 'Error':
             failed += 1
         done += 1
         if not done % 20:
             btn = [[
                 InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#groups')
             ]]
-            await sts.edit(f"Groups groadcast in progress...\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>", reply_markup=InlineKeyboardMarkup(btn))    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+            await sts.edit(f"Groups broadcast in progress...\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>", reply_markup=InlineKeyboardMarkup(btn))
+    
+    time_taken = datetime.timedelta(seconds=int(time.time() - start_time))
     await sts.edit(f"Groups broadcast completed.\nCompleted in {time_taken} seconds.\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
+
