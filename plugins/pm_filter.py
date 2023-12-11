9b171f1ff1eaf4c697353f0dffc1be31f24caebb
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2023-12-11 18:24:03 +0530

    updated line 2234

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2231,7 +2231,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ]
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
-    await query.answer(MSG_ALRT)
+        else:
+            await query.message.edit_text(MSG_ALRT)
+
 
     
 async def auto_filter(client, msg, spoll=False):
