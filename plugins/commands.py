e6d9a27e991d0c06d79419057eda44c4783e4d3c
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2023-12-26 11:49:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -805,7 +805,8 @@ async def deletemultiplefiles(bot, message):
     await asyncio.sleep(30)
     deleted = 0
     for file in files:
-        await k.edit_text(f"<b>Process started for deleting files from DB. Successfully deleted <code>{str(deleted)}/{total}</code> files from DB for your query <code>{keyword}</code> !\n\nPlease wait...</b>",
+    if not  deleted % 20:
+        await message.edit(f"<b>Process started for deleting files from DB. Successfully deleted <code>{str(deleted)}/{total}</code> files from DB for your query <code>{keyword}</code> !\n\nPlease wait...</b>",
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🚸 ᴅᴇʟᴇᴛᴇ', callback_data='close_data')]]))
         file_ids = file.file_id                   
         file_name = file.file_name
