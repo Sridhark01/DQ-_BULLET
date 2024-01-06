3957be5c428792891c320ba70b11fac446619413
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2024-01-06 06:57:00 +0530

    Update animeme.py

--- plugins/more2/animeme.py
+++ plugins/more2/animeme.py
@@ -1,18 +1,17 @@
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import Client, filters
-import requests
-
-# These codes were created by @lallu_tgs
-
-@Client.on_message(filters.command(["ameme", "animememe"]))
-async def animememes(_, m):
-    try:
-        res = requests.get("https://meme-api.herokuapp.com/gimme/animememes").json()
-        url = res['url']
-        text = res.get('title', 'No Title')
-        link = res['postLink']
+import random
+import requests 
         
-        await m.reply_photo(url, caption=f"[{text}]({link})", reply_markup=InlineKeyboardMarkup(
+#these codes created by @lallu_tgs
+      
+@Client.on_message(filters.command(["ameme","animememe"]))
+async def animememes(_, m):
+     res = requests.get("https://meme-api.herokuapp.com/gimme/animememes").json()
+     url = res['url']
+     text = res['title']
+     link = res['postLink']
+     await m.reply_photo(url,caption=f"[{text}]({link})",reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
@@ -21,14 +20,9 @@ async def animememes(_, m):
                     ),
                 ],
             ],
-        ))
-    
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching anime meme: {e}")
-        # Handle the error, log it, or take appropriate action.
-        await m.reply_text("Sorry, an error occurred while fetching the anime meme. Please try again later.")
-
-
+        ),
+    )
+        
 @Client.on_callback_query(filters.regex("ameme"))
 async def ameme(_, query: CallbackQuery):
                    query = query.message
