12ab738a958f9623e7570edf18352ce2adb2e319
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2025-04-26 08:50:46 +0530

    Update info.py
    changes in api id hash and admin id

--- info.py
+++ info.py
@@ -16,9 +16,9 @@ def is_enabled(value, default):
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(environ['API_ID',"13859375"])
+API_HASH = environ['API_HASH',"6ae4ba5fdc4eb0948616585a5bb7ee58"]
+BOT_TOKEN = environ['BOT_TOKEN',"6372243428:AAH0_ZcT7t-MiSuQnIiVSNit6TBZegYw3CE"]
 USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
 
 #Clone
@@ -62,7 +62,7 @@ AI_LOGS = int(environ.get("AI_LOGS","-1001774912790")) #GIVE YOUR NEW LOG CHANNE
 
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6327489457').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6327489457 2034319320').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001979254627 -1001530438262 -1001979254627 -1001891660326 -1001973908906 -1001908151821').split()] # alpha dump channel id -1001973908906
 
 LANGUAGES = ["malayalam", "tamil", "english", "hindi", "telugu", "kannada"]
