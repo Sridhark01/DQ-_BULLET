0c8066a20be97818421cf44a6f98cae3d762fd1f
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2025-10-22 11:45:53 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -2,6 +2,7 @@ import logging
 from struct import pack
 import re
 import base64
+from asyncio import coroutine
 from pyrogram.file_id import FileId
 from pymongo.errors import DuplicateKeyError
 from umongo import Instance, Document, fields
